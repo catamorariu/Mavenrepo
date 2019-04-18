03db7511e3b6648070331b82798a0b5a933ba40f
catamorariu <49591432+catamorariu@users.noreply.github.com>
2019-04-18 18:03:36 +0300

    Add files via upload

---
+++ pip/src/main/java/pip/Pipeline3.java
@@ -0,0 +1,14 @@
+package pip;
+
+public class Pipeline3 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Worksssss!!!!!!!!");
+		System.out.println("test git status");
+		System.out.println("test git statusssgit status");
+		System.out.println("test git statusssgitt status");
+		
+	}
+
+}
